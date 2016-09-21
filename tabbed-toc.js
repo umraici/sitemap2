141493a8a2c82c09f7df53648ba100f343f173f6
umraici <aldirayci74@gmail.com>
2016-09-22 02:59:44 +0700

    Create tabbed-toc.js

---
+++ tabbed-toc.js
@@ -0,0 +1,40 @@
+<style>
+tabbed-toc {
+width:100%; margin:0 auto; background-color:#09C; overflow:hidden; position:relative; color:#333 }
+tabbed-toc .loading {
+display:block; padding:5px 10px; font-family:'Open Sans',Sans-Serif; font-weight:400; font-size:10px; color:#fff; }
+tabbed-toc ul,#tabbed-toc ol,#tabbed-toc li {
+margin:0; padding:0; list-style:none }
+tabbed-toc .toc-tabs {
+width:25%; float:left } 1. abbed-toc .toc-tabs li a { display:block; font-family:Electrolize,ge_ss_threeregular; font-weight:400; font-size:10px; height:31px; overflow:hidden; text-overflow:ellipsis; color:#fff; text-transform:uppercase; text-decoration:none; padding:6px 12px; cursor:pointer; } 1. abbed-toc .toc-tabs li a:hover { background-color:#1083A9; color:#fff }
+tabbed-toc .toc-tabs li a.active-tab {
+background-color:#747474; color:#fff; position:relative; z-index:5; margin:0 -1px 0 0 }
+tabbed-toc .toc-content,#tabbed-toc .divider-layer {
+width:75%; float:right; background-color:#fff; border-left:5px solid #747474; box-sizing:border-box }
+tabbed-toc .divider-layer {
+float:none; display:block; position:absolute; top:0; right:0; bottom:0; } 1. abbed-toc .panel { position:relative; z-index:5; font-family:'Open Sans',Sans-Serif; font-weight:400; font-size:10px; }
+tabbed-toc .panel li a {
+display:block; position:relative; font-weight:700; font-size:11px; color:#7E7C7C; line-height:30px; height:30px; padding:6px 12px; text-decoration:none; outline:none; font-family: Electrolize,droid arabic kufi; overflow:hidden }
+tabbed-toc .panel li a em {
+background:#F03232; color:#fff!important; padding:2px 4px; border-radius:3px; font-style:normal; }
+tabbed-toc .panel li time {
+display:block; font-style:italic; font-weight:400; font-size:10px;
+color:#666; float:right }
+tabbed-toc .panel li .summary {
+display:block; padding:10px 12px; font-style:italic; border-bottom:4px solid #275827; overflow:hidden } 1. abbed-toc .panel li .summary img.thumbnail { float:left; display:block; margin:0 8px 0 0; padding:4px; width:72px; height:72px; border:1px solid #dcdcdc; background-color:#fafafa }
+tabbed-toc .panel li:nth-child(even) {
+background-color:#f0f0f0; font-size:10px;
+color:#fff }
+tabbed-toc .panel li:nth-child(odd) {
+background-color:#f9f9f9; font-size:10px; color:#fff }
+tabbed-toc .panel li a:hover,#tabbed-toc .panel li a:focus,#tabbed-toc .panel li a:hover time,#tabbed-toc .panel li.bold a {
+background-color:#747474; color:#fff; outline:none }
+tabbed-toc .panel li.bold a:hover,#tabbed-toc .panel li.bold a:hover time {
+background-color:#444; }
+</style>
+جاري التحميل...
+<script type="text/javascript">
+var tabbedTOC = { blogUrl: "http://www.areakost.net/", // Blog URL containerId: "tabbed-toc", // Container ID activeTab: 1, // The default active tab index (default: the first tab) showDates: false, // true to show the post date showSummaries: false, // true to show the posts summaries numChars: 200, // Number of summary chars showThumbnails: false, // true to show the posts thumbnails (Not recommended) thumbSize: 40, // Thumbnail size noThumb: "http://3.bp.blogspot.com/-vpCFysMEZys/UOEhSGjkfnI/AAAAAAAAFwY/h1wuA5kfEhg/s72-c/grey.png", // A "no thumbnail" URL monthNames: // Array of month names "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" , newTabLink: true, // Open link in new window? maxResults: 99999, // Maximum post results preload: 0, // Load the feed after 0 seconds (option => time in milliseconds || "onload") sortAlphabetically: true, // false to sort posts by published date showNew: 7, // false to hide the "جديد!" mark in most recent posts, or define how many recent posts are to be marked newText: " - جديد!" // HTML for the "جديد!" text };
+</script>
+<script src="https://ar1web-com.googlecode.com/svn/Hm/sitemapv2.js" type="text/javascript">
+</script>
